b22250671bcde099fa61f9d0862ca005329ed9e7
Harshil Valiya <harshilvaliya40@gmail.com>
2024-09-03 14:57:52 +0530

    login icon in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,45 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const navIcons = [
-  { src: "/assets/icons/search.svg", alt: "search" },
-  { src: "/assets/icons/black-heart.svg", alt: "heart" },
-  { src: "/assets/icons/user.svg", alt: "user" },
-];
-
 const Navbar = () => {
   return (
-    <header className="w-full">
-      <nav className="nav">
-        <Link
-          href="/"
-          className="flex items-center justify-center w-full gap-1"
-        >
+    <header className="w-full flex items-center justify-between p-4">
+      {/* Logo Section */}
+      <Link href="/" className="flex items-center gap-1">
+        <Image src="/assets/icons/logo.svg" width={40} height={40} alt="logo" />
+        <p className="nav-logo">
+          Deal<span className="text-primary"> Scout</span>
+        </p>
+      </Link>
+
+      {/* Icons and Login Section */}
+      <div className="flex items-center gap-5">
+        <Link href="/login" className="text-primary flex items-center gap-1">
           <Image
-            src="/assets/icons/logo.svg"
-            width={40}
-            height={40}
-            alt="logo"
+            src="/assets/icons/user.svg"
+            alt="login"
+            width={28}
+            height={28}
+            className="object-contain"
           />
-
-          <p className="nav-logo">
-            Deal<span className="text-primary"> Scout</span>
-          </p>
+          <span>Login</span>
         </Link>
-
-        {/* <div className="flex items-center gap-5">
-          {navIcons.map((icon) => (
-            <Image
-              key={icon.alt}
-              src={icon.src}
-              alt={icon.alt}
-              width={28}
-              height={28}
-              className="object-contain"
-            />
-          ))}
-        </div> */}
-      </nav>
+      </div>
     </header>
   );
 };
